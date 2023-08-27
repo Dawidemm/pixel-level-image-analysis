587cde07815607de0a7207fa5cd3bc11c4989a49
Dawidemm <dawid.mazur@icloud.com>
2023-08-27 20:33:06 +0200

    Update mnist

--- mnist.py
+++ mnist.py
@@ -1,5 +1,4 @@
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
@@ -7,19 +6,17 @@ import matplotlib.pyplot as plt
 from lbae import LBAE
 from pipeline import Pipeline
 
-# Load the digits dataset
-
-def plot_digits(nrows, ncols, images, targets, preds=None):
-    fig, axes = plt.subplots(nrows, ncols, figsize=(8, 6))
-    for i in range(nrows):
-        for j in range(ncols):
-            axes[i, j].imshow(images[i*ncols + j], cmap='gray')
-            axes[i, j].axis('off')
-            if preds == None:
-                axes[i, j].set_title(f'Label:{targets[i*ncols + j]}')
-            else:
-                axes[i, j].set_title(f'Label:{targets[i*ncols + j]}\nPredict:{preds[i*ncols + j]}')
-    plt.show()
+# def plot_digits(nrows, ncols, images, targets, preds=None):
+#     fig, axes = plt.subplots(nrows, ncols, figsize=(8, 6))
+#     for i in range(nrows):
+#         for j in range(ncols):
+#             axes[i, j].imshow(images[i*ncols + j], cmap='gray')
+#             axes[i, j].axis('off')
+#             if preds == None:
+#                 axes[i, j].set_title(f'Label:{targets[i*ncols + j]}')
+#             else:
+#                 axes[i, j].set_title(f'Label:{targets[i*ncols + j]}\nPredict:{preds[i*ncols + j]}')
+#     plt.show()
 
 digits = datasets.load_digits()
 
@@ -27,16 +24,15 @@ digits = datasets.load_digits()
 # ncols = 5
 # plot_digits(nrows, ncols, digits.images, digits.target)
 
-# Split into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.2)
+dataset_split = int(len(digits.data) * 0.8)
 
-X_train = torch.Tensor(X_train)
-X_test = torch.Tensor(X_test)
-y_train = torch.Tensor(y_train)
-y_test = torch.Tensor(y_test)
+X_train, X_test = digits.data[:dataset_split], digits.data[dataset_split:]
+y_train, y_test = digits.target[:dataset_split], digits.target[dataset_split:]
 
-train_dataloader = DataLoader(list(zip(X_train,y_train)), batch_size=1, shuffle=True)
-test_dataloader = DataLoader(list(zip(X_test,y_test)), batch_size=1, shuffle=False)
+X_train, X_test = torch.Tensor(X_train), torch.Tensor(X_test)
+y_train, y_test = torch.Tensor(y_train), torch.Tensor(y_test)
+
+train_dataloader = DataLoader(list(zip(X_train,y_train)), batch_size=1, shuffle=False)
 
 NUM_VISIBLE = 60
 MAX_EPOCHS = 35
