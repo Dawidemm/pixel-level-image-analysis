28cb98c27126664faf06981b4e1eef066a4f0d0b
Dawidemm <dawid.mazur@icloud.com>
2023-08-27 20:33:16 +0200

    Update mnist_eval

--- mnist_eval.py
+++ mnist_eval.py
@@ -4,37 +4,44 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+from sklearn.metrics import rand_score
 
 from lbae import LBAE
 
-model = LBAE.load_from_checkpoint('E:/projects/pixel-level-image-analysis/lightning_logs/version_28/checkpoints/epoch=71-step=103464.ckpt')
-model.eval()
+model = LBAE.load_from_checkpoint(checkpoint_path='/Users/dawidmazur/Code/pixel-level-image-analysis/lightning_logs/version_3/checkpoints/epoch=19-step=28740.ckpt', 
+                                  hparams_file='/Users/dawidmazur/Code/pixel-level-image-analysis/lightning_logs/version_3/hparams.yaml',
+                                  map_location=torch.device('cpu'))
 
 digits = datasets.load_digits()
-X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.2)
+dataset_split = int(len(digits.data) * 0.8)
 
-print(X_test.shape)
+X_train, X_test = digits.data[:dataset_split], digits.data[dataset_split:]
+y_train, y_test = digits.target[:dataset_split], digits.target[dataset_split:]
 
-X_test = torch.Tensor(X_test)
-y_test = torch.Tensor(y_test)
+X_train, X_test = torch.Tensor(X_train), torch.Tensor(X_test)
+y_train, y_test = torch.Tensor(y_train), torch.Tensor(y_test)
 
-X_test = torch.reshape(X_test, (360, 1, 8, 8))
+X_test = torch.reshape(X_test, (len(X_test), 1, 8, 8))
 
-test_dataloader = DataLoader(list(zip(X_test,y_test)), batch_size=1, shuffle=False)
+preds = model(X_test)
 
-with torch.inference_mode(): 
-    y_preds = model(X_test)
-    print(y_preds)
+test_first_image = X_test[0].clone().detach()
+preds_first_image = preds[0].clone().detach()
 
-# def plot_digits(nrows, ncols, images, targets, preds=None):
-#     fig, axes = plt.subplots(nrows, ncols, figsize=(8, 6))
-#     for i in range(nrows):
-#         for j in range(ncols):
-#             axes[i, j].imshow(images[i*ncols + j], cmap='gray')
-#             axes[i, j].axis('off')
-#             if preds == None:
-#                 axes[i, j].set_title(f'Label:{targets[i*ncols + j]}')
-#             else:
-#                 axes[i, j].set_title(f'Label:{targets[i*ncols + j]}\nPredict:{preds[i*ncols + j]}')
-#     plt.show()
+# plt.imshow(test_first_image.permute(1, 2, 0), cmap='gray')
+# plt.show()
 
+# plt.imshow(preds_first_image.permute(1, 2, 0), cmap='gray')
+# plt.show()
+
+test_first_image = test_first_image.reshape(64,)
+preds_first_image = preds_first_image.reshape(64,)
+
+rand_index = rand_score(test_first_image, preds_first_image)
+print(f'Rand score dla pierwszego elementu z zbioru testowwego: {round(rand_index, 2)}')
+
+X_test = X_test.clone().detach().reshape(360*64,)
+preds = preds.clone().detach().reshape(360*64,)
+
+rand_index_test = rand_score(X_test, preds)
+print(f'Rand score dla zbioru testowwego: {round(rand_index_test, 2)}')
