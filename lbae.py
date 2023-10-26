de6c9ef634d4e44d33656b8d705a040418272142
Dawidemm <dawid.mazur@icloud.com>
2023-10-26 09:59:25 +0200

    Update lbae.py

--- lbae.py
+++ lbae.py
@@ -4,8 +4,8 @@ import lightning as pl
 import torch.nn.functional as F
 from torch import optim
 
-from .decoder import LBAEDecoder
-from .encoder import LBAEEncoder
+from decoder import LBAEDecoder
+from encoder import LBAEEncoder
 
 
 def loss(xr, x):
