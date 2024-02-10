9a7f79d1efbcc1448acfe40b6751baa2c298f31f
Dawidemm <dawid.mazur@icloud.com>
2024-02-10 17:14:25 +0100

    feat: Change number of visible and hidden neurons.

--- train_pipeline_hyperspectral.py
+++ train_pipeline_hyperspectral.py
@@ -1,6 +1,7 @@
 from torch.utils.data import DataLoader
 from hyperspectral_dataset import HyperspectralDataset
 import torch
+import numpy as np
 
 from lbae import LBAE
 from pipeline import Pipeline
@@ -8,27 +9,30 @@ from rbm import RBM
 
 torch.set_float32_matmul_precision('medium')
 
-NUM_VISIBLE = 60
-NUM_HIDDEN = 40
+np.random.seed(10)
+torch.manual_seed(0)
 
-MAX_EPOCHS = 50
+NUM_VISIBLE = 16
+NUM_HIDDEN = 17
+
+MAX_EPOCHS = 100
 RBM_STEPS = 1000
 BATCH_SIZE = 8
 
 def main():
 
-    dataset = HyperspectralDataset(
+    train_dataset = HyperspectralDataset(
     hyperspectral_image_path='dataset/hyperspectral_image.tif',
     ground_truth_image_path='dataset/ground_truth_image.tif',
     stage='train'
     )
 
-    train_dataloader = DataLoader(dataset=dataset, batch_size=8, shuffle=False)
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=8, shuffle=True, num_workers=4)
 
     autoencoder = LBAE(input_size=(1, 16, 16), out_channels=8, zsize=NUM_VISIBLE, num_layers=2, quantize=list(range(MAX_EPOCHS)))
     rbm = RBM(NUM_VISIBLE, NUM_HIDDEN)
 
-    pipeline = Pipeline(auto_encoder=autoencoder, rbm=True, classifier=True)
+    pipeline = Pipeline(auto_encoder=autoencoder, rbm=rbm, classifier=True)
 
     pipeline.fit(train_dataloader, max_epochs=MAX_EPOCHS)
 
