fd0002bf68adb0ba968ee450a9aab8d8fa2e45f9
Dawidemm <dawid.mazur@icloud.com>
2024-03-27 18:00:02 +0100

    feat: Adjust training parameters.

--- src/scripts/hyperspectral_train_pipeline.py
+++ src/scripts/hyperspectral_train_pipeline.py
@@ -12,36 +12,36 @@ torch.set_float32_matmul_precision('medium')
 np.random.seed(10)
 torch.manual_seed(0)
 
-NUM_VISIBLE = 16
-NUM_HIDDEN = 17
+NUM_VISIBLE = 60
+NUM_HIDDEN = 30
 
-MAX_EPOCHS = 100
+MAX_EPOCHS = 25
 RBM_STEPS = 1000
-BATCH_SIZE = 8
+BATCH_SIZE = 32
 
-HYPERSPECTRAL_IMAGE_PATH = 'dataset/hyperspectral_image.tif'
-GROUND_TRUTH_IMAGE_PATH = 'dataset/ground_truth_image.tif'
+HYPERSPECTRAL_IMAGE_PATH = 'dataset/indian_pine/220x2678x614/hyperspectral_image.tif'
+GROUND_TRUTH_IMAGE_PATH = 'dataset/indian_pine/220x2678x614/ground_truth_image.tif'
 
 def main():
 
     train_dataset = HyperspectralDataset(
-        hyperspectral_image_path=HYPERSPECTRAL_IMAGE_PATH,
-        ground_truth_image_path=GROUND_TRUTH_IMAGE_PATH,
+        hyperspectral_data=HYPERSPECTRAL_IMAGE_PATH,
+        ground_truth_data=GROUND_TRUTH_IMAGE_PATH,
         stage=Stage.TRAIN
     )
 
     train_dataloader = DataLoader(
         dataset=train_dataset,
-        batch_size=8, 
+        batch_size=64, 
         shuffle=True, 
         num_workers=4
     )
 
     autoencoder = LBAE(
-        input_size=(1, 16, 16),
-        out_channels=8, 
+        input_size=(1, 220),
+        out_channels=16, 
         latent_size=NUM_VISIBLE,
-        num_layers=2,
+        num_layers=4,
         quantize=list(range(MAX_EPOCHS))
     )
     
