a71a1ca2fde41daf2c25a0faabaf29718bc6a295
Dawidemm <dawid.mazur@icloud.com>
2024-04-23 21:34:10 +0200

    feat: Test separate different return data parameters.

--- tests/test_hyperspectral_dataset.py
+++ tests/test_hyperspectral_dataset.py
@@ -9,23 +9,16 @@ def hyperspectral_dataset_train():
     ground_truth_data = np.random.randint(0, 16, size=(1, 145, 145))
     return HyperspectralDataset(hyperspectral_data, ground_truth_data, Stage.TRAIN)
 
-@pytest.fixture
-def hyperspectral_dataset_test():
-    hyperspectral_data = np.random.rand(220, 145, 145)
-    ground_truth_data = np.random.randint(0, 16, size=(1, 145, 145))
-    return HyperspectralDataset(hyperspectral_data, ground_truth_data, Stage.TEST)
-
-def test_hyperspectral_dataset_getitem(hyperspectral_dataset_train, hyperspectral_dataset_test):
+def test_hyperspectral_dataset_getitem_type(hyperspectral_dataset_train):
     sample_index = 0
     pixel_values_train, label_train = hyperspectral_dataset_train[sample_index]
-    pixel_values_test, label_test = hyperspectral_dataset_test[sample_index]
 
     assert isinstance(pixel_values_train, torch.Tensor)
     assert isinstance(label_train, torch.Tensor)
-    assert pixel_values_train.shape == torch.Size([1, 220])
-    assert label_train.shape == torch.Size([])
 
-    assert isinstance(pixel_values_test, torch.Tensor)
-    assert isinstance(label_test, torch.Tensor)
-    assert pixel_values_test.shape == torch.Size([1, 220])
-    assert label_test.shape == torch.Size([])
+def test_hyperspectral_dataset_getitem_shape(hyperspectral_dataset_train):
+    sample_index = 0
+    pixel_values, label = hyperspectral_dataset_train[sample_index]
+
+    assert pixel_values.shape == torch.Size([1, 220])
+    assert label.shape == torch.Size([])
