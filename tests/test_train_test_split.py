d855216fe0aad3cd5f45e89ff6e706127f194e5c
Dawidemm <dawid.mazur@icloud.com>
2024-04-23 21:27:25 +0200

    feat: Add pytest.fixture to test multiple split params.

--- tests/test_train_test_split.py
+++ tests/test_train_test_split.py
@@ -5,6 +5,10 @@ import pytest
 HYPERSPECTRAL_IMAGE = np.random.rand(220, 145, 145)
 GROUND_TRUTH_IMAGE = np.random.randint(0, 16, size=(1, 145, 145))
 
+@pytest.fixture(params=[0.1, 0.15, 0.2, 0.25, 0.3])
+def split(request):
+    return request.param
+
 def test_train_test_split_output():
     dataset = utils.train_test_split(HYPERSPECTRAL_IMAGE, GROUND_TRUTH_IMAGE)
 
@@ -18,21 +22,19 @@ def test_dimension_mismatch_error():
     with pytest.raises(ValueError):
         utils.train_test_split(HYPERSPECTRAL_IMAGE, np.random.rand(1, 50, 50))
 
-def test_multiple_splits():
-    splits_to_test = [0.1, 0.15, 0.2, 0.25, 0.3]
-    for split in splits_to_test:
-        _, _, ground_truth_train, ground_truth_test = utils.train_test_split(HYPERSPECTRAL_IMAGE, GROUND_TRUTH_IMAGE, split=split)
-        expected_split = len(ground_truth_test)/(len(ground_truth_train)+len(ground_truth_test))
+def test_multiple_splits(split):
+    _, _, ground_truth_train, ground_truth_test = utils.train_test_split(HYPERSPECTRAL_IMAGE, GROUND_TRUTH_IMAGE, split=split)
+    expected_split = len(ground_truth_test)/(len(ground_truth_train)+len(ground_truth_test))
 
-        assert np.isclose(split, expected_split, atol=0.01)
+    assert np.isclose(split, expected_split, atol=0.01)
 
 def test_class_balance():
-    split = 0.35
+    split = 0.2
     dataset = utils.train_test_split(HYPERSPECTRAL_IMAGE, GROUND_TRUTH_IMAGE, split=split)
     _, _, _, removed_ground_truth = dataset
 
     classes_in_ground_truth_image = np.unique(GROUND_TRUTH_IMAGE)
-    total_samples_per_class = {class_pixel_value: len(np.argwhere(GROUND_TRUTH_IMAGE== class_pixel_value)) for class_pixel_value in classes_in_ground_truth_image}
+    total_samples_per_class = {class_pixel_value: len(np.argwhere(GROUND_TRUTH_IMAGE == class_pixel_value)) for class_pixel_value in classes_in_ground_truth_image}
 
     for class_pixel_value in classes_in_ground_truth_image:
         total_samples = total_samples_per_class[class_pixel_value]
