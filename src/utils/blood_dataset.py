dafb06b33ee2fbb89ebdfe2a418555071ff439b2
Dawidemm <dawid.mazur@icloud.com>
2024-07-15 16:52:11 +0200

    feat: Migrate data loading to IterableDataset.

--- src/utils/blood_dataset.py
+++ src/utils/blood_dataset.py
@@ -2,61 +2,15 @@ import os
 import torch
 import numpy as np
 import spectral.io.envi as envi
-from torch.utils.data import Dataset
-from typing import Tuple, Sequence
+from torch.utils.data import IterableDataset
+from typing import Sequence
 
-
-def blood_pixel_generator(
-        hyperspectral_data_path: str,
-        ground_truth_data_path: str,
-):
-    
-    hyperspectral_data_files = os.listdir(hyperspectral_data_path)
-
-    hyperspectral_files = [f for f in hyperspectral_data_files if os.path.isfile(os.path.join(hyperspectral_data_path, f))]
-
-    float_files = []
-    hdr_files = []
-
-    for file in hyperspectral_files:
-        if file.endswith('.float'):
-            float_files.append(file)
-        elif file.endswith('.hdr'):
-            hdr_files.append(file)
-
-    if len(float_files) == len(hdr_files):
-        number_of_images = len(float_files)
-
-    float_files = sorted(float_files)
-    hdr_files = sorted(hdr_files)
-
-    ground_truth_data_files = os.listdir(ground_truth_data_path)
-
-    ground_truth_files = [f for f in ground_truth_data_files if os.path.isfile(os.path.join(ground_truth_data_path, f))]
-    ground_truth_files = sorted(ground_truth_files)
-
-    for i in range(number_of_images):
-
-        img = envi.open(f'{hyperspectral_data_path}/{hdr_files[i]}', f'{hyperspectral_data_path}/{float_files[i]}')
-        img = np.asarray(img[:,:,:], dtype=np.float32)
-
-        gt = np.load(f'{ground_truth_data_path}/{ground_truth_files[i]}')
-        gt = np.asarray(gt['gt'][:,:], dtype=np.float32)
-        gt[gt > 7] = 0
-
-        if img.shape[:2] == gt.shape:
-            rows, cols, bands = img.shape
-
-        for row in range(rows):
-            for col in range(cols):
-                yield torch.tensor(img[row, col]), torch.tensor(gt[row, col])
 
 def blood_dataset_params(
         hyperspectral_data_path: str,
         ground_truth_data_path: str,
 ):
     pixel_max_value = 0
-    dataset_length = 0
     classes = 0
 
     hyperspectral_data_files = os.listdir(hyperspectral_data_path)
@@ -92,8 +46,6 @@ def blood_dataset_params(
         if img.max() >= pixel_max_value:
             pixel_max_value = img.max()
 
-        dataset_length += rows * cols
-
         gt = np.load(f'{ground_truth_data_path}/{ground_truth_files[i]}')
         gt = np.asarray(gt['gt'][:,:], dtype=np.float32)
         gt[gt > 7] = 0
@@ -101,36 +53,68 @@ def blood_dataset_params(
         if len(np.unique(gt)) >= classes:
             classes = len(np.unique(gt))
 
-    return pixel_max_value, dataset_length, classes    
+    return pixel_max_value, classes    
 
-class BloodDataset(Dataset):
+
+class BloodIterableDataset(IterableDataset):
     def __init__(
             self,
             hyperspectral_data_path: str,
             ground_truth_data_path: str,
     ):
-        self.pixel_max_value, self.dataset_length, self.classes = blood_dataset_params(
+        self.hyperspectral_data_path = hyperspectral_data_path
+        self.ground_truth_data_path = ground_truth_data_path
+
+        self.pixel_max_value, self.classes = blood_dataset_params(
             hyperspectral_data_path=hyperspectral_data_path,
             ground_truth_data_path=ground_truth_data_path
         )
 
-        self.blood_generator = blood_pixel_generator(
-            hyperspectral_data_path=hyperspectral_data_path,
-            ground_truth_data_path=ground_truth_data_path
-        )
+    def __iter__(self):
 
-    def  __len__(self):
-        return self.dataset_length
-    
-    def __getitem__(self, index: int) -> Tuple[Sequence, int]:
+        hyperspectral_data_files = os.listdir(self.hyperspectral_data_path)
+        hyperspectral_files = [f for f in hyperspectral_data_files if os.path.isfile(os.path.join(self.hyperspectral_data_path, f))]
 
-        pixel, label = next(self.blood_generator)
-        pixel = pixel/self.pixel_max_value
-        pixel = pixel.reshape(1, pixel.shape[0])
-        label = self.onehot_encoding(int(label.item()))
-        
-        return pixel, label
-    
+        float_files = []
+        hdr_files = []
+
+        for file in hyperspectral_files:
+            if file.endswith('.float'):
+                float_files.append(file)
+            elif file.endswith('.hdr'):
+                hdr_files.append(file)
+
+        if len(float_files) == len(hdr_files):
+            number_of_images = len(float_files)
+
+        float_files = sorted(float_files)
+        hdr_files = sorted(hdr_files)
+
+        ground_truth_data_files = os.listdir(self.ground_truth_data_path)
+
+        ground_truth_files = [f for f in ground_truth_data_files if os.path.isfile(os.path.join(self.ground_truth_data_path, f))]
+        ground_truth_files = sorted(ground_truth_files)
+
+        for i in range(number_of_images):
+
+            img = envi.open(f'{self.hyperspectral_data_path}/{hdr_files[i]}', f'{self.hyperspectral_data_path}/{float_files[i]}')
+            img = np.asarray(img[:,:,:], dtype=np.float32)
+
+            gt = np.load(f'{self.ground_truth_data_path}/{ground_truth_files[i]}')
+            gt = np.asarray(gt['gt'][:,:], dtype=np.float32)
+            gt[gt > 7] = 0
+
+            if img.shape[:2] == gt.shape:
+                rows, cols, bands = img.shape
+
+            for row in range(rows):
+                for col in range(cols):
+                    pixel = torch.tensor(img[row, col]) / self.pixel_max_value
+                    pixel = pixel.reshape(1, pixel.shape[0])
+                    label = torch.tensor(gt[row, col])
+                    label = self.onehot_encoding(int(label.item()))
+                    yield pixel, label
+
     def onehot_encoding(self, label: torch.TensorType) -> Sequence[int]:
         onehot_label = torch.zeros(self.classes)
         onehot_label[label] = 1.0
