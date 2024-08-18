74ee6ff7a39bfb9f92624fbb1b34e65574907c2e
Dawidemm <dawid.mazur@icloud.com>
2024-08-18 13:23:20 +0200

    feat: Change script to run.

--- slurm/pixellevel.sh
+++ slurm/pixellevel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH -J RBM_for_pixel-level_multispectral_image_segmentation
+#SBATCH -J blood_lbae_experiments
 #SBATCH -A plgrbm4heo-cpu
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
@@ -14,5 +14,4 @@ ml miniconda3
 eval "$(conda shell.bash hook)"
 conda activate pixellevel
 
-## srun python ~/pixel-level-image-analysis/src/scripts/hyperspectral_blood_train_pipeline.py
-srun python ~/pixel-level-image-analysis/exp_13/blood_clustering.py
+srun python ~/pixel-level-image-analysis/src/scripts/blood/blood_lbae_experiments_pipeline.py
