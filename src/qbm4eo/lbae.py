3bfb93dcd526bfff7d92fb2cdf7007d2c64c5e24
Dawidemm <dawid.mazur@icloud.com>
2024-08-22 18:51:00 +0200

    feat: Add learning rate as hyperparameter.

--- src/qbm4eo/lbae.py
+++ src/qbm4eo/lbae.py
@@ -2,7 +2,6 @@ from typing import Any
 import torch
 import lightning as pl
 from torch.nn.functional import mse_loss
-from torch import optim
 
 from src.qbm4eo.decoder import LBAEDecoder
 from src.qbm4eo.encoder import LBAEEncoder
@@ -10,13 +9,15 @@ from src.qbm4eo.encoder import LBAEEncoder
 
 class LBAE(pl.LightningModule):
     def __init__(
-        self, input_size, out_channels, latent_size, num_layers, quantize, *args: Any, **kwargs: Any):
+        self, input_size, out_channels, latent_size, num_layers, learning_rate, quantize, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         
         self.save_hyperparameters('input_size', 'out_channels', 'latent_size', 'num_layers', 'quantize')
 
         self.encoder = LBAEEncoder(input_size, out_channels, latent_size, num_layers, quantize)
         self.decoder = LBAEDecoder(self.encoder.final_conv_size, input_size, latent_size, num_layers)
+
+        self.learning_rate = learning_rate
         self.epoch = 0
 
     def forward(self, x):
@@ -54,4 +55,5 @@ class LBAE(pl.LightningModule):
         return mse_loss(xr.view(x.size()), x, reduction="sum")
 
     def configure_optimizers(self):
-        return {"optimizer": optim.Adam(self.parameters(), lr=1e-3)}
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
