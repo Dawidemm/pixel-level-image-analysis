ebd5ca67e5945e8ae5de4e545ded9861741471dd
Dawidemm <dawid.mazur@icloud.com>
2024-08-24 13:58:03 +0200

    feat: Add SLURM config file for rbm experiments.

---
+++ slurm/blood_rbm_experiments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH -J blood_rbm_experiments
+#SBATCH -A plgrbm4heo-cpu
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=5GB
+#SBATCH --time=72:00:00
+#SBATCH --mail-type=END
+#SBATCH --error="error.txt"
+#SBATCH --output="output.txt"
+#SBATCH -p plgrid
+
+ml miniconda3
+eval "$(conda shell.bash hook)"
+conda activate pixellevel
+
+srun python ~/pixel-level-image-analysis/src/scripts/blood/blood_rbm_experiments_pipeline.py
