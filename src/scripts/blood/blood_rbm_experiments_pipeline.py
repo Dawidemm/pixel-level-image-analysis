d82d76239bd8c4a750715b5a971d9df8c52ed7da
Dawidemm <dawid.mazur@icloud.com>
2024-08-24 17:25:11 +0200

    feat: Set up rbm experiments.

--- src/scripts/blood/blood_rbm_experiments_pipeline.py
+++ src/scripts/blood/blood_rbm_experiments_pipeline.py
@@ -1,38 +1,35 @@
 from torch.utils.data import DataLoader
 from src.utils.blood_dataset import BloodIterableDataset, Stage
+from src.utils import utils
 import torch
 import numpy as np
-import time
 import os
-
-from src.utils import utils
 
 from src.qbm4eo.lbae import LBAE
 from src.qbm4eo.pipeline import Pipeline
 from src.qbm4eo.rbm import RBM
+
 
 torch.set_float32_matmul_precision('medium')
 
 np.random.seed(10)
 torch.manual_seed(0)
 
-NUM_VISIBLE = 32
+NUM_VISIBLE = 28
+NUM_HIDDEN = 8
 
+BATCH_SIZE = [8, 16, 32]
 NUM_HIDDEN = [3, 8, 16]
-RBM_STEPS = [1000, 10000]
 RBM_LEARNING_RATE = [0.001, 0.0001]
-
-IMAGES = ['D_1', 'E_1', 'F_1']
-
-BATCH_SIZE = 16
 
 THRESHOLDS = np.linspace(1/10, 1, 10)[:-1]
 
 HYPERSPECTRAL_DATA_PATH = 'HyperBlood/data'
 GROUND_TRUTH_DATA_PATH = 'HyperBlood/anno'
+IMAGES = ['D_1', 'E_1', 'F_1']
 
-AUTOENCODER_CHECKPOINT_PATH = 'model/epoch=14-step=4410900.ckpt'
-AUTOENCODER_HPARAMS_PATH = 'model/hparams.yaml'
+AUTOENCODER_CHECKPOINT_PATH = 'lightning_logs/version_1/checkpoints/epoch=9-step=72570.ckpt'
+AUTOENCODER_HPARAMS_PATH = 'lightning_logs/version_1/hparams.yaml'
 
 EXPERIMENT_FOLDER_PATH = './experiments/'
 
@@ -40,87 +37,94 @@ def main():
 
     os.makedirs(EXPERIMENT_FOLDER_PATH, exist_ok=True)
     with open(EXPERIMENT_FOLDER_PATH+'experiments_raport.csv', 'a+') as file:
-        file.write(f'time,image,experiment,num_hidden,rbm_steps,rbm_lr,threshold,ari,rand_score,homogenity,completeness\n')
+        file.write(f'experiment,batch_size,num_hidden\n')
 
     experiment = 0
 
-    for image in IMAGES:
+    for learning_rate in RBM_LEARNING_RATE:
+        for batch_size in BATCH_SIZE:
+            for num_hidden in NUM_HIDDEN:
 
-        train_dataset = BloodIterableDataset(
-            hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
-            ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
-            load_specific_image=image,
-            remove_noisy_bands=False,
-            stage=Stage.TRAIN
-        )
+                train_dataset = BloodIterableDataset(
+                    hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
+                    ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
+                    load_specific_images=IMAGES,
+                    stage=Stage.TRAIN,
+                    remove_noisy_bands=True,
+                    remove_background=True,
+                    shuffle=True
+                )
 
-        find_threshold_dataset = BloodIterableDataset(
-            hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
-            ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
-            load_specific_image=image,
-            remove_noisy_bands=False,
-        )
+                val_dataset = BloodIterableDataset(
+                    hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
+                    ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
+                    load_specific_images=IMAGES,
+                    stage=Stage.VAL,
+                    remove_noisy_bands=True,
+                    remove_background=True,
+                    shuffle=True
+                )
 
-        train_dataloader = DataLoader(
-            dataset=train_dataset,
-            batch_size=BATCH_SIZE,
-            num_workers=0,
-        )
+                test_dataset = BloodIterableDataset(
+                    hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
+                    ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
+                    load_specific_images=IMAGES,
+                    stage=Stage.TEST,
+                    remove_noisy_bands=True,
+                    remove_background=True,
+                    shuffle=False
+                )
 
-        find_threshold_dataloader = DataLoader(
-            dataset=find_threshold_dataset,
-            batch_size=1,
-            num_workers=0,
-        )
+                train_dataloader = DataLoader(
+                    dataset=train_dataset,
+                    batch_size=batch_size
+                )
 
-        lbae = LBAE.load_from_checkpoint(
-            checkpoint_path=AUTOENCODER_CHECKPOINT_PATH,
-            hparams_file=AUTOENCODER_HPARAMS_PATH,
-            map_location=torch.device('cpu')
-        )
-    
-        for num_hidden in NUM_HIDDEN:
-            for rbm_steps in RBM_STEPS:
-                for rbm_lr in RBM_LEARNING_RATE:
+                val_dataloader = DataLoader(
+                    dataset=val_dataset,
+                    batch_size=batch_size
+                )
 
-                    rbm = RBM(NUM_VISIBLE, num_hidden)
+                test_dataloader = DataLoader(
+                    dataset=test_dataset,
+                    batch_size=batch_size
+                )
 
-                    pipeline = Pipeline(auto_encoder=lbae, rbm=rbm)
+                lbae = LBAE.load_from_checkpoint(
+                    checkpoint_path=AUTOENCODER_CHECKPOINT_PATH,
+                    hparams_file=AUTOENCODER_HPARAMS_PATH,
+                    map_location=torch.device('cpu')
+                )
 
-                    print(f'Experiment nr: {experiment}\n')
-                    print(f'Start Time: {time.ctime()}\n')
-                    print(f'Experiment settings:\n')
-                    print(f'\tImage: {image},\n')
-                    print(f'\tNum Hidden: {num_hidden},\n')
-                    print(f'\tRBM steps: {rbm_steps},\n')
-                    print(f'\tRBM learning rate: {rbm_lr}.\n')
+                rbm = RBM(NUM_VISIBLE, num_hidden)
 
-                    pipeline.fit(
-                        train_dataloader,
-                        skip_autoencoder=True,
-                        rbm_steps=rbm_steps,
-                        rbm_learning_rate=rbm_lr, 
-                        rbm_trainer='cd1', 
-                        learnig_curve=True,
-                        experiment_folder_path=EXPERIMENT_FOLDER_PATH,
-                        experiment_number=experiment
-                    )
+                pipeline = Pipeline(auto_encoder=lbae, rbm=rbm)
 
-                    rbm = RBM(NUM_VISIBLE, num_hidden)
-                    rbm.load(file=f'{EXPERIMENT_FOLDER_PATH}exp_{experiment}/rbm.npz')
+                pipeline.fit(
+                    train_data_loader=train_dataloader,
+                    validation_data_loader=val_dataloader,
+                    skip_autoencoder=True,
+                    rbm_trainer='cd1',
+                    rbm_learning_rate=learning_rate,
+                    learnig_curve=True,
+                    experiment_folder_path=EXPERIMENT_FOLDER_PATH,
+                    experiment_number=experiment
+                )
 
-                    threshold_finder = utils.ThresholdFinder(
-                        dataloader=find_threshold_dataloader,
-                        encoder=lbae.encoder,
-                        rbm=rbm
-                    )
+                rbm = RBM(NUM_VISIBLE, num_hidden)
+                rbm.load(file=f'./experiments/exp_{experiment}/rbm.npz')
+                threshold_finder = utils.ThresholdFinder(
+                    dataloader=test_dataloader,
+                    encoder=lbae.encoder,
+                    rbm=rbm
+                )
 
-                    threshold, ari, rand_score, homogenity, completeness, _  = threshold_finder.find_threshold(THRESHOLDS)
+                threshold, ari, rand_score, homogeneity, completeness = threshold_finder.find_threshold(THRESHOLDS)
 
-                    with open(EXPERIMENT_FOLDER_PATH+'experiments_raport.csv', 'a+') as file:
-                        file.write(f'{experiment},{image},{num_hidden},{rbm_steps},{rbm_lr},{threshold},{ari},{rand_score},{homogenity},{completeness}\n')
+                with open(EXPERIMENT_FOLDER_PATH+'experiments_raport.csv', 'a+') as file:
+                    file.write(f'{experiment},{batch_size},{learning_rate},{num_hidden},{threshold},{ari},{rand_score},{homogeneity},{completeness}\n')
 
-                    experiment += 1
+                experiment += 1
 
 if __name__ == '__main__':
     main()
