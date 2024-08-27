700ed9f8b960311f077ae411c6deaf7cd91f8ad2
Dawidemm <dawid.mazur@icloud.com>
2024-08-28 00:28:28 +0200

    chore: Placeholder commit.

--- src/scripts/blood/blood_segmentation.py
+++ src/scripts/blood/blood_segmentation.py
@@ -1,97 +1,97 @@
+import os
 import torch
 import numpy as np
 from torch.utils.data import DataLoader
+from sklearn.cluster import KMeans, HDBSCAN
+from sklearn.metrics import completeness_score, homogeneity_score
+import matplotlib.pyplot as plt
+
 from src.utils.blood_dataset import BloodIterableDataset, Stage
+from src.utils import utils
 
 from src.qbm4eo.lbae import LBAE
 from src.qbm4eo.rbm import RBM
 
-from src.utils import utils
-
-from torchmetrics.functional.pairwise import pairwise_euclidean_distance
-
 np.random.seed(10)
 torch.manual_seed(0)
 
-NUM_VISIBLE = 28
-NUM_HIDDEN = 8
 
 HYPERSPECTRAL_DATA_PATH = 'HyperBlood/data'
 GROUND_TRUTH_DATA_PATH = 'HyperBlood/anno'
 
-AUTOENCODER_CHECKPOINT_PATH = 'ares2/pixel-level-image-analysis/lightning_logs/version_0/checkpoints/epoch=9-step=2940600.ckpt'
-AUTOENCODER_HPARAMS_PATH = 'ares2/pixel-level-image-analysis/lightning_logs/version_0/hparams.yaml'
+AUTOENCODER_CHECKPOINT_PATH = 'lightning_logs/version_0/checkpoints/epoch=19-step=58060.ckpt'
+AUTOENCODER_HPARAMS_PATH = 'lightning_logs/version_0/hparams.yaml'
 
-RBM_WEIGHTS_PATH = 'ares2/pixel-level-image-analysis/rbm.npz'
+IMAGES = ['D_1', 'E_1', 'F_1']
 
-THRESHOLDS = np.linspace(1/10, 1, 10)
+BACKGROUND_VALUE = 0
 
 def main():
-    try:
-        
-        blood_dataset = BloodIterableDataset(
-            hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH ,
-            ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
-            num_images_to_load=1,
-            stage=Stage.IMG_SEG
+
+    test_dataset = BloodIterableDataset(
+        hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
+        ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
+        load_specific_images=IMAGES,
+        stage=Stage.TEST,
+        remove_noisy_bands=True,
+        remove_background=True
+    )
+
+    test_dataloader = DataLoader(
+            dataset=test_dataset,
+            batch_size=1,
+            drop_last=True
         )
 
-        blood_dataloader = DataLoader(
-            dataset=blood_dataset,
-            batch_size=1
-        )
-
-        lbae = LBAE.load_from_checkpoint(
-            checkpoint_path=AUTOENCODER_CHECKPOINT_PATH, 
-            hparams_file=AUTOENCODER_HPARAMS_PATH,
-            map_location=torch.device('cpu')
-        )
-
-        rbm = RBM(NUM_VISIBLE, NUM_HIDDEN)
-        rbm.load(file=RBM_WEIGHTS_PATH)
-
-        
-    except FileNotFoundError as e:
-        print(f'FileNotFoundError: {e}')
-        print("Please make sure to:\n"
-              "\t1. Provide paths to the hyperspectral image and ground truth image files.\n"
-              "\t2. Run the training pipeline before starting the evaluation.\n"
-              "The application will terminate now.")
-        return
+    lbae = LBAE.load_from_checkpoint(
+        checkpoint_path=AUTOENCODER_CHECKPOINT_PATH, 
+        hparams_file=AUTOENCODER_HPARAMS_PATH,
+        map_location=torch.device('cpu')
+    )
 
     lbae.eval()
 
-    mean_distances = []
+    y_true = []
+    hidden_representations = []
 
     with torch.no_grad():
-        for X, _ in blood_dataloader:
-            prediction = lbae(X)
-            prediction = prediction.reshape(prediction.shape[0]*prediction.shape[2], 1)
-            X = X.reshape(X.shape[0]*X.shape[2], 1)
-            distance = pairwise_euclidean_distance(X, prediction)
-            mean_distances.append(torch.mean(distance))
-
-    print(f'\n---------------------------------------------')
-    print(f'Autoencoder')
-    print(f'Pairwise euclidean distance: {round(torch.mean(torch.tensor(mean_distances)).item(), 3)}.')
-
-    threshold_finder = utils.ThresholdFinder(
-        dataloader=blood_dataloader,
-        encoder=lbae.encoder,
-        rbm=rbm
-    )
-
-    best_threshold, adjusted_rand_score, rand_score, rbm_homogeneity, rbm_completeness = threshold_finder.find_threshold(THRESHOLDS)
-
-    print(f'\n---------------------------------------------')
-    print(f'LBAE+RBM')
-    print(f'Best threshold: {round(best_threshold, 3)}.')
-    print(f'Adjusted rand score: {round(adjusted_rand_score, 3)}.')
-    print(f'Rand score: {round(rand_score, 3)}.')
+        for X, y in test_dataloader:
+            hidden_representation, _ = lbae.encoder(X, epoch=1)
+            hidden_representation = hidden_representation.detach().numpy()
     
-    print(f'\nLBAE+RBM Clustering:')
-    print(f'Homogenity score: {round(rbm_homogeneity, 3)}')
-    print(f'Completeness score: {round(rbm_completeness, 3)}\n')
+            y_true.append(y)
+            hidden_representations.append(hidden_representation)
+
+    y_true = np.concatenate(y_true)
+
+    hidden_representations = np.concatenate(hidden_representations)
+
+        # lbae_hdbscan = HDBSCAN()
+        # lbae_hdbscan.fit(hidden_representations)
+        # lbae_hdbscan_clustering = lbae_hdbscan.labels_
+
+        # lbae_hdbscan_homogeneity = homogeneity_score(y_true, lbae_hdbscan_clustering)
+        # lbae_hdbscan_completeness = completeness_score(y_true, lbae_hdbscan_clustering)
+
+    lbae_kmeans = KMeans(n_clusters=7)
+    lbae_kmeans.fit(hidden_representations)
+    lbae_kmeans_clustering = lbae_kmeans.labels_
+        
+    lbae_kmeans_homogeneity = homogeneity_score(y_true, lbae_kmeans_clustering)
+    lbae_kmeans_completeness = completeness_score(y_true, lbae_kmeans_clustering)
+        
+        # with open(f'lbae_{image}_metrics.txt', 'a+') as file:
+        #     file.write(f'LBAE+HDBSCAN Clustering:\n')
+        #     file.write(f'Homogenity score: {round(lbae_hdbscan_homogeneity, 3)}.\n')
+        #     file.write(f'Completeness score: {round(lbae_hdbscan_completeness, 3)}.\n')
+
+        #     file.write(f'LBAE+Kmeans Clustering:\n')
+        #     file.write(f'Homogenity score: {round(lbae_kmeans_homogeneity, 3)}.\n')
+        #     file.write(f'Completeness score: {round(lbae_kmeans_completeness, 3)}.\n')
+
+    print(f'LBAE+Kmeans Clustering:\n')
+    print(f'Homogenity score: {round(lbae_kmeans_homogeneity, 3)}.\n')
+    print(f'Completeness score: {round(lbae_kmeans_completeness, 3)}.\n')
 
 if __name__ == '__main__':
     main()
