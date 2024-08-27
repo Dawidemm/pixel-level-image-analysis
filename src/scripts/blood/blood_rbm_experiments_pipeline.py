dc0010c3b766de25d3c0890719ac32e77da4b46b
Dawidemm <dawid.mazur@icloud.com>
2024-08-28 00:29:46 +0200

    feat: Set up for new experiments.

--- src/scripts/blood/blood_rbm_experiments_pipeline.py
+++ src/scripts/blood/blood_rbm_experiments_pipeline.py
@@ -12,15 +12,14 @@ from src.qbm4eo.rbm import RBM
 
 torch.set_float32_matmul_precision('medium')
 
-np.random.seed(10)
-torch.manual_seed(0)
+torch.manual_seed(10)
 
 NUM_VISIBLE = 28
-NUM_HIDDEN = 8
 
-BATCH_SIZE = [8, 16, 32]
-NUM_HIDDEN = [3, 8, 16]
+BATCH_SIZE = [4, 8, 16]
+NUM_HIDDEN = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 RBM_LEARNING_RATE = [0.001, 0.0001]
+RANDOM_SEEDS = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 
 THRESHOLDS = np.linspace(1/10, 1, 10)[:-1]
 
@@ -28,8 +27,8 @@ HYPERSPECTRAL_DATA_PATH = 'HyperBlood/data'
 GROUND_TRUTH_DATA_PATH = 'HyperBlood/anno'
 IMAGES = ['D_1', 'E_1', 'F_1']
 
-AUTOENCODER_CHECKPOINT_PATH = 'lightning_logs/version_4/checkpoints/epoch=14-step=54420.ckpt'
-AUTOENCODER_HPARAMS_PATH = 'lightning_logs/version_4/hparams.yaml'
+AUTOENCODER_CHECKPOINT_PATH = 'model/epoch=19-step=290280.ckpt'
+AUTOENCODER_HPARAMS_PATH = 'model/hparams.yaml'
 
 EXPERIMENT_FOLDER_PATH = './experiments/'
 
@@ -37,98 +36,109 @@ def main():
 
     os.makedirs(EXPERIMENT_FOLDER_PATH, exist_ok=True)
     with open(EXPERIMENT_FOLDER_PATH+'experiments_raport.csv', 'a+') as file:
-        file.write(f'experiment,batch_size,num_hidden\n')
+        file.write(f'experiment,batch_size,learning_rate,num_hidden,random_state,threshold,ari,rand_score,homogeneity,completeness\n')
 
     experiment = 0
 
     for learning_rate in RBM_LEARNING_RATE:
         for batch_size in BATCH_SIZE:
             for num_hidden in NUM_HIDDEN:
+                for random_seed in RANDOM_SEEDS:
 
-                train_dataset = BloodIterableDataset(
-                    hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
-                    ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
-                    load_specific_images=IMAGES,
-                    stage=Stage.TRAIN,
-                    remove_noisy_bands=True,
-                    remove_background=True,
-                    shuffle=True
-                )
+                    train_dataset = BloodIterableDataset(
+                        hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
+                        ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
+                        load_specific_images=IMAGES,
+                        stage=Stage.TRAIN,
+                        remove_noisy_bands=True,
+                        remove_background=True
+                    )
 
-                val_dataset = BloodIterableDataset(
-                    hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
-                    ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
-                    load_specific_images=IMAGES,
-                    stage=Stage.VAL,
-                    remove_noisy_bands=True,
-                    remove_background=True,
-                    shuffle=True
-                )
+                    val_dataset = BloodIterableDataset(
+                        hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
+                        ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
+                        load_specific_images=IMAGES,
+                        stage=Stage.VAL,
+                        remove_noisy_bands=True,
+                        remove_background=True
+                    )
 
-                test_dataset = BloodIterableDataset(
-                    hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
-                    ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
-                    load_specific_images=IMAGES,
-                    stage=Stage.TEST,
-                    remove_noisy_bands=True,
-                    remove_background=True,
-                    shuffle=False
-                )
+                    test_dataset = BloodIterableDataset(
+                        hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
+                        ground_truth_data_path=GROUND_TRUTH_DATA_PATH,
+                        load_specific_images=IMAGES,
+                        stage=Stage.TEST,
+                        remove_noisy_bands=True,
+                        remove_background=True
+                    )
 
-                train_dataloader = DataLoader(
-                    dataset=train_dataset,
-                    batch_size=batch_size,
-                    drop_last=True
-                )
+                    train_dataloader = DataLoader(
+                        dataset=train_dataset,
+                        batch_size=batch_size,
+                        drop_last=True
+                    )
 
-                val_dataloader = DataLoader(
-                    dataset=val_dataset,
-                    batch_size=batch_size,
-                    drop_last=True
-                )
+                    val_dataloader = DataLoader(
+                        dataset=val_dataset,
+                        batch_size=batch_size,
+                        drop_last=True
+                    )
 
-                test_dataloader = DataLoader(
-                    dataset=test_dataset,
-                    batch_size=1,
-                    drop_last=True
-                )
+                    test_dataloader = DataLoader(
+                        dataset=test_dataset,
+                        batch_size=1,
+                        drop_last=True
+                    )
 
-                lbae = LBAE.load_from_checkpoint(
-                    checkpoint_path=AUTOENCODER_CHECKPOINT_PATH,
-                    hparams_file=AUTOENCODER_HPARAMS_PATH,
-                    map_location=torch.device('cpu')
-                )
+                    lbae = LBAE.load_from_checkpoint(
+                        checkpoint_path=AUTOENCODER_CHECKPOINT_PATH,
+                        hparams_file=AUTOENCODER_HPARAMS_PATH,
+                        map_location=torch.device('cpu')
+                    )
 
-                rbm = RBM(NUM_VISIBLE, num_hidden)
+                    rbm = RBM(
+                        num_visible=NUM_VISIBLE,
+                        num_hidden=num_hidden,
+                        random_seed=random_seed
+                    )
 
-                pipeline = Pipeline(auto_encoder=lbae, rbm=rbm)
+                    pipeline = Pipeline(
+                        auto_encoder=lbae, 
+                        rbm=rbm
+                    )
 
-                pipeline.fit(
-                    train_data_loader=train_dataloader,
-                    validation_data_loader=val_dataloader,
-                    skip_autoencoder=True,
-                    rbm_trainer='cd1',
-                    rbm_learning_rate=learning_rate,
-                    learnig_curve=True,
-                    experiment_folder_path=EXPERIMENT_FOLDER_PATH,
-                    experiment_number=experiment
-                )
+                    pipeline.fit(
+                        train_data_loader=train_dataloader,
+                        validation_data_loader=val_dataloader,
+                        skip_autoencoder=True,
+                        rbm_trainer='cd1',
+                        rbm_learning_rate=learning_rate,
+                        rbm_epochs=1,
+                        learnig_curve=True,
+                        experiment_folder_path=EXPERIMENT_FOLDER_PATH,
+                        experiment_number=experiment
+                    )
 
-                rbm = RBM(NUM_VISIBLE, num_hidden)
-                rbm.load(file=f'./experiments/exp_{experiment}/rbm.npz')
+                    rbm = RBM(
+                        num_visible=NUM_VISIBLE,
+                        num_hidden=num_hidden,
+                        random_seed=random_seed
+                    )
 
-                threshold_finder = utils.ThresholdFinder(
-                    dataloader=test_dataloader,
-                    encoder=lbae.encoder,
-                    rbm=rbm
-                )
+                    rbm.load(file=f'./experiments/exp_{experiment}/rbm.npz')
 
-                threshold, ari, rand_score, homogeneity, completeness = threshold_finder.find_threshold(THRESHOLDS)
+                    threshold_finder = utils.ThresholdFinder(
+                        dataloader=test_dataloader,
+                        encoder=lbae.encoder,
+                        rbm=rbm
+                    )
 
-                with open(EXPERIMENT_FOLDER_PATH+'experiments_raport.csv', 'a+') as file:
-                    file.write(f'{experiment},{batch_size},{learning_rate},{num_hidden},{threshold},{ari},{rand_score},{homogeneity},{completeness}\n')
+                    threshold, ari, rand_score, homogeneity, completeness, _ = threshold_finder.find_threshold(THRESHOLDS)
 
-                experiment += 1
+                    with open(EXPERIMENT_FOLDER_PATH+'experiments_raport.csv', 'a+') as file:
+                        file.write(f'{experiment},{batch_size},{learning_rate},{num_hidden},{random_seed},{threshold},{ari},{rand_score},{homogeneity},{completeness}\n')
+
+                    experiment += 1
 
 if __name__ == '__main__':
     main()
