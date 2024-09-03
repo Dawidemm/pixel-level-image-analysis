d36fc8a12ee1a48158cb1c88655a0d08f0967215
Dawidemm <dawid.mazur@icloud.com>
2024-09-03 19:53:29 +0200

    feat: Set up Kmeans experiments.

--- src/scripts/blood/blood_segmentation.py
+++ src/scripts/blood/blood_segmentation.py
@@ -2,12 +2,10 @@ import os
 import torch
 import numpy as np
 from torch.utils.data import DataLoader
-from sklearn.cluster import KMeans, HDBSCAN
+from sklearn.cluster import KMeans
 from sklearn.metrics import completeness_score, homogeneity_score
-import matplotlib.pyplot as plt
 
 from src.utils.blood_dataset import BloodIterableDataset, Stage
-from src.utils import utils
 
 from src.qbm4eo.lbae import LBAE
 from src.qbm4eo.rbm import RBM
@@ -19,14 +17,22 @@ torch.manual_seed(0)
 HYPERSPECTRAL_DATA_PATH = 'HyperBlood/data'
 GROUND_TRUTH_DATA_PATH = 'HyperBlood/anno'
 
-AUTOENCODER_CHECKPOINT_PATH = 'lightning_logs/version_0/checkpoints/epoch=19-step=58060.ckpt'
-AUTOENCODER_HPARAMS_PATH = 'lightning_logs/version_0/hparams.yaml'
+AUTOENCODER_CHECKPOINT_PATH = 'model/epoch=19-step=290280.ckpt'
+AUTOENCODER_HPARAMS_PATH = 'model/hparams.yaml'
 
 IMAGES = ['D_1', 'E_1', 'F_1']
 
+EXPERIMENT_FOLDER_PATH = './kmeans/'
+
 BACKGROUND_VALUE = 0
 
+RANDOM_SEEDS = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+
 def main():
+
+    os.makedirs(EXPERIMENT_FOLDER_PATH, exist_ok=True)
+    with open(EXPERIMENT_FOLDER_PATH+'experiments_raport.csv', 'a+') as file:
+        file.write(f'experiment,random_state,homogeneity,completeness\n')
 
     test_dataset = BloodIterableDataset(
         hyperspectral_data_path=HYPERSPECTRAL_DATA_PATH,
@@ -54,6 +60,8 @@ def main():
     y_true = []
     hidden_representations = []
 
+    X_true = []
+
     with torch.no_grad():
         for X, y in test_dataloader:
             hidden_representation, _ = lbae.encoder(X, epoch=1)
@@ -62,36 +70,30 @@ def main():
             y_true.append(y)
             hidden_representations.append(hidden_representation)
 
+            X_true.append(X)
+
     y_true = np.concatenate(y_true)
 
     hidden_representations = np.concatenate(hidden_representations)
 
-        # lbae_hdbscan = HDBSCAN()
-        # lbae_hdbscan.fit(hidden_representations)
-        # lbae_hdbscan_clustering = lbae_hdbscan.labels_
+    X_true = np.concatenate(X_true)
+    X_true = X_true.reshape(X_true.shape[0], X_true.shape[2])
 
-        # lbae_hdbscan_homogeneity = homogeneity_score(y_true, lbae_hdbscan_clustering)
-        # lbae_hdbscan_completeness = completeness_score(y_true, lbae_hdbscan_clustering)
+    experiment = 0
 
-    lbae_kmeans = KMeans(n_clusters=7)
-    lbae_kmeans.fit(hidden_representations)
-    lbae_kmeans_clustering = lbae_kmeans.labels_
-        
-    lbae_kmeans_homogeneity = homogeneity_score(y_true, lbae_kmeans_clustering)
-    lbae_kmeans_completeness = completeness_score(y_true, lbae_kmeans_clustering)
-        
-        # with open(f'lbae_{image}_metrics.txt', 'a+') as file:
-        #     file.write(f'LBAE+HDBSCAN Clustering:\n')
-        #     file.write(f'Homogenity score: {round(lbae_hdbscan_homogeneity, 3)}.\n')
-        #     file.write(f'Completeness score: {round(lbae_hdbscan_completeness, 3)}.\n')
+    for random_seed in RANDOM_SEEDS:
 
-        #     file.write(f'LBAE+Kmeans Clustering:\n')
-        #     file.write(f'Homogenity score: {round(lbae_kmeans_homogeneity, 3)}.\n')
-        #     file.write(f'Completeness score: {round(lbae_kmeans_completeness, 3)}.\n')
+        lbae_kmeans = KMeans(n_clusters=7, random_state=random_seed)
+        lbae_kmeans.fit(X_true)
+        lbae_kmeans_clustering = lbae_kmeans.labels_
+            
+        homogeneity = homogeneity_score(y_true, lbae_kmeans_clustering)
+        completeness = completeness_score(y_true, lbae_kmeans_clustering)
 
-    print(f'LBAE+Kmeans Clustering:\n')
-    print(f'Homogenity score: {round(lbae_kmeans_homogeneity, 3)}.\n')
-    print(f'Completeness score: {round(lbae_kmeans_completeness, 3)}.\n')
+        with open(EXPERIMENT_FOLDER_PATH+'experiments_raport.csv', 'a+') as file:
+            file.write(f'{experiment},{random_seed},{homogeneity},{completeness}\n')
+
+        experiment += 1
 
 if __name__ == '__main__':
     main()
